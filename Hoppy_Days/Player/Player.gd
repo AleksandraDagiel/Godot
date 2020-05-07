008643b1d0ef9ba4ea58bab657da3f76b8a9f281
Aleksandra <naoki.kircia@gmail.com>
2020-05-07 19:31:36 +0200

    hoppydays beginning

--- Hoppy_Days/Player/Player.gd
+++ Hoppy_Days/Player/Player.gd
@@ -2,28 +2,36 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
-const SPEED = 1000
-const GRAVITY = 300
+const SPEED = 1500
+const GRAVITY = 150
 const UP = Vector2(0, -1)
-const JUMP_SPEED = 3000
+const JUMP_SPEED = 3500
+const WORLD_LIMIT = 4000
+
+signal animate
 
 func _physics_process(delta):
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion, UP)
 	 
 
 
 func apply_gravity():
+	if position.y > WORLD_LIMIT:
+		end_game()
 	if is_on_floor():
 		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
 	else:
 		motion.y += GRAVITY
 
 
 func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED\
 
 
@@ -34,4 +42,21 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
-	
+
+
+func animate():
+	emit_signal("animate", motion)
+#	if motion.y <0:
+#		$PlayerAnimation.play("jump")
+#	elif motion.x > 0:
+#		$PlayerAnimation.set_flip_h(false)
+#		$PlayerAnimation.play("walk")
+#	elif motion.x < 0:
+#		$PlayerAnimation.set_flip_h(true)
+#		$PlayerAnimation.play("walk")
+#	else:
+#		$PlayerAnimation.play("idle")
+#
+
+func end_game():
+	get_tree().change_scene("res://Levels/GameOver.tscn")
